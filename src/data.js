5025bc9c46dec8d0d6fcbb6c061b67e1bb82c447
Layton5 <kayodematthieu@gmail.com>
2022-11-04 03:18:21 +0100

    new

---
+++ src/data.js
@@ -0,0 +1,55 @@
+let shopItemsData = [
+  {
+    id: "A",
+    name: "Casual Slide",
+    price: "10000",
+    desc: "Goes well on Native",
+    img: "images/palm1.jfif",
+  },
+  {
+    id: "B",
+    name: "Birken-stock",
+    price: "8000",
+    desc: "Goes well on Native",
+    img: "images/palm1.jfif",
+  },
+  {
+    id: "C",
+    name: "mule",
+    price: "6500",
+    desc: "Goes well on Native",
+    img: "images/palm2.jfif",
+  },
+
+  {
+    id: "E",
+    name: "Addidas",
+    price: "5000",
+    desc: "Goes well on Native",
+    img: "images/slide1.jfif",
+  },
+
+  {
+    id: "F",
+    name: "Gucci",
+    price: "7000",
+    desc: "Goes well on Native",
+    img: "images/slide2.jfif",
+  },
+
+  {
+    id: "G",
+    name: "Nike",
+    price: "1200",
+    desc: "Goes well on Native",
+    img: "images/slide3.jfif",
+  },
+
+  {
+    id: "H",
+    name: "flip",
+    price: "6000",
+    desc: "Goes well on Native",
+    img: "images/slide5.jfif",
+  },
+];
